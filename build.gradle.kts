e464ffe23ddd732ad552f2ca520f669d82f386ca
Abdi10salam <mrcabdisalaam@gmail.com>
2025-03-11 08:11:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,11 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id "org.sonarqube" version "4.0.0.2929" // Use the latest version
 }
+sonarqube {
+    properties {
+        property "sonar.projectKey", "sonar-server_project5"
+        property "sonar.organization", "sonar-server"
+        property "sonar.host.url", "https://sonarcloud.io"
+    }
+}
+
